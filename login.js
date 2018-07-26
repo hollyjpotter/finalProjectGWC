18da4e162e5d53138bace905422b4b795f91a578
hollyjpotter <41168067+hollyjpotter@users.noreply.github.com>
2018-07-26 10:05:33 -0700

    Update login.js

--- login.js
+++ login.js
@@ -3,4 +3,6 @@ function onSignIn(googleUser)
     var profile = googleUser.getBasicProfile();
     $(".g-signin2").css("display","none");
     $("data").css("display","blocl");
+    $("#pic").attr('scr',profile.getImageUrl());
+    $("#email").text(profile.getEmail());
 }
