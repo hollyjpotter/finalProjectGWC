fb32a408e763c530a1269eb377a928046eb28414
arthiv21 <arthiv21@gmail.com>
2018-07-27 10:50:24 -0700

    Countdown

--- script.js
+++ script.js
@@ -2,34 +2,40 @@ var fridge = [];
 var date = [];
 var days = [];
 
-function calendar() {
 
-  // page is now ready, initialize the calendar...
-    //document.getElementById("calendar").innerHTML = "It has been reached";
-
-  $('#calendar').fullCalendar({
-    // put your options and callbacks here
-  })
-
+var dict = {
+  'apple': 1,
+  'banana': 6
 };
 
 function addFood(){
   var food = prompt("Type the name of the food you want to add");
-  var date2 = prompt("Type the dat that food expires");
-  var date1 = '7/24/2018';
+  var date2 = prompt("Type the date that food expires", "M/D/YY or idk");
+  var date1 = '7/26/2018';
   var i = fridge.length + 0;
   fridge.push(food);
-  date.push(date2);
-  
+
   dt1 = new Date(date1);
   dt2 = new Date(date2);
   var diff = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) -
     Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /
     (1000 * 60 * 60 * 24));
-  days.push(diff)
-  document.getElementById("dateDiff").innerHTML =
-  "Days left: " + days;
+
+  if (date2 == 'idk'){
+    for (var key in dict){
+      if (key == food){
+        var myDate = new Date(new Date().getTime()+(dict[key]*24*60*60*1000));
+        date.push(myDate)
+        days.push(dict[key])
+      }
+    }
+  }
+  else {
+    date.push(new Date(date2));
+    days.push(diff)
+  }
 }
+
 
 
 function create_table(){
@@ -38,6 +44,7 @@ function create_table(){
     table += '<tr>';
       table += '<td>' + fridge[i] + '</td>';
       table += '<td>' + date[i] + '</td>';
+      table += '<td>' + days[i] + '</td>';
     table += '</tr>';
   }
   document.getElementById("tab").innerHTML = ('<table>' + table + '</table>')
