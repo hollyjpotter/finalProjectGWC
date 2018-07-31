d2a85df804ddd6a7e017c3b6378bf81609c47716
arthiv21 <arthiv21@gmail.com>
2018-07-31 11:15:23 -0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,9 @@
 var events = [
-  {'Date': new Date(2018, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
-  {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
-  {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+  {'Date': new Date(2018, 7, 7), 'Title': 'Tomatoes'},
+  {'Date': new Date(2018, 8, 18), 'Title': 'Fish', 'Link': 'https://garfield.com'},
+  {'Date': new Date(2018, 7, 27), 'Title': 'Watermelon', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
 ];
+
 var settings = {};
 var element = document.getElementById('caleandar');
 caleandar(element, events, settings);
